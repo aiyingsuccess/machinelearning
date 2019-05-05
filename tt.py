265351acf5b8c261e5f36611eed25d4256115c81
ying <1120019687@qq.com>
2019-05-05 16:01:37 -0400

    sun

--- tt.py
+++ tt.py
@@ -1,13 +1,33 @@
+import pandas as pd
 import numpy as np
-from math import pow
-import random
 
-numbers = [1,2,3,4,5,1,4,5] 
-  
-# start parameter is not provided 
-Sum = sum(numbers) 
-print(Sum) 
-  
-# start = 10 
-Sum = sum(numbers, 10) 
-print(Sum) 
+df = pd.DataFrame(np.random.randn(5, 3), index=['a', 'c', 'e', 'f', 'h'],
+                 columns=['one', 'two', 'three']) 
+
+df['four'] = 'bar'
+
+df['five'] = df['one'] > 0
+print(df)
+df2 = df.reindex(['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'])
+print(df2)
+print(df2['one'])
+print(pd.isna(df2['one']['a']))
+print(type(df2['one']['a']))
+dt=df.fillna(0)
+values = set(dt['one'])
+print('val',values)
+df = pd.DataFrame([[np.nan, 2, np.nan, 0],
+                    [3, 4, np.nan, 1],
+                    [np.nan, np.nan, np.nan, 5],
+                    [np.nan, 3, np.nan, 4]],
+                 columns=list('ABCD'))
+
+print(df)
+
+# dt=df.fillna('ss')
+
+# print(dt)
+
+values = set(df['A'])
+print(values)
+
